03d97f659d15ae552ade3e1a51e756c188bc4591
Rakib Hasan <rakib_me@hotmail.com>
2021-07-07 10:58:47 +0600

    piramid-2

---
+++ piramid-2.c
@@ -0,0 +1,29 @@
+/*
+Print the following piramid
+* * * * * 
+  * * * * 
+    * * * 
+      * * 
+        * 
+*/
+
+#include<stdio.h>
+int main()
+{
+    int n, i, j;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(i=n; i>=1; i--)
+    {
+        for(j=1; j<=(n-i); j++)
+        {
+            printf("  ");
+        }
+        for(j=i; j>=1; j--) 
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
