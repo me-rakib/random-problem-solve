991d3f136d62038109a9b407780f9e11cd30c6ce
Tanaf <tanaf.dev@gmail.com>
2021-08-08 19:54:57 +0600

    codeforces 462A

--- 462A-appleman-and-easy-task.cpp
+++ 462A-appleman-and-easy-task.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 int main()
 {
-    int n, c, i, j;
+    int n, i, j, c;
     cin >> n;
-    char ch[n][n];
     bool even = true;
-
+    char ch[n][n];
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
@@ -14,33 +13,42 @@ int main()
             cin >> ch[i][j];
         }
     }
-
     for (i = 0; i < n; i++)
     {
         c = 0;
         for (j = 0; j < n; j++)
         {
             if (i - 1 >= 0 and ch[i - 1][j] == 'o')
+            {
                 c++;
+            }
             if (i + 1 < n and ch[i + 1][j] == 'o')
+            {
                 c++;
+            }
             if (j - 1 >= 0 and ch[i][j - 1] == 'o')
+            {
                 c++;
+            }
             if (j + 1 < n and ch[i][j + 1] == 'o')
+            {
                 c++;
+            }
             if (c % 2 == 1)
             {
                 even = false;
+                break;
             }
+        }
+        if (even == false)
+        {
+            cout << "NO" << endl;
+            break;
         }
     }
     if (even)
     {
-        cout << "YES";
-    }
-    else
-    {
-        cout << "NO";
+        cout << "YES" << endl;
     }
     return 0;
 }
